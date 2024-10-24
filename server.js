1ed739255b3e935e170f1524337914e4f407afc4
Addition <joeladdition@gmail.com>
2024-10-24 15:41:19 +0000

    first commit

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 const express = require('express');
+const cors = require('cors'); 
+const morgan = require('morgan'); 
 
-const studentRoutes = require("./src/routes")
+const studentRoutes = require("./src/routes");
 const app = express();
 
 const PORT = process.env.PORT || 2000;
 
+// CORS options
+const corsOptions = {
+  origin: 'http://example.com', // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow credentials (cookies, authorization headers)
+};
 
-//middle ware..
-app.use(express.json())
+// Middleware
+app.use(cors(corsOptions)); 
+app.use(morgan('dev')); 
+app.use(express.json()); 
 
 // GET method route
 app.get('/', (req, res) => {
-    res.send('Welcome to Home Page')
-  });
-  
+  res.send('Welcome to Home Page');
+});
 
-  //
-  app.use('/user/student', studentRoutes)
+// Define routes
+app.use('/user/student', studentRoutes);
 
-
-  //PORT
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
